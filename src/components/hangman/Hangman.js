2c5498cae768ea7dfc68e79cb3701c0628720109
jas112 <jas.sandmaster@gmail.com>
2022-07-15 02:06:22 -0700

    first commit on react-hangman-exercise

---
+++ src/components/hangman/Hangman.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Hangman.css';
+
+class Hangman extends Component {
+  render() {
+    return (
+        <div className='Hangman-container'>
+            <h1>Hangman Exercise</h1>
+        </div>
+    )
+  }
+}
+
+export default Hangman;
